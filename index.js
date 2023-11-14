f841d45353720e59090ee0742ab906d2b9844b7f
Michelot Alexis <amichelot@club-internet.fr>
2023-11-14 21:44:56 +0100

    added cors middleware globaly

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 const Offer = require("./models/Offer");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI);
 
