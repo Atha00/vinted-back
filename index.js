926a3c64dedf8fa0dde14f0a944245919c0fe34a
Michelot Alexis <amichelot@club-internet.fr>
2023-11-14 21:32:07 +0100

    added env PORT

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.all("*", (req, res) => {
   return res.status(404).json("Not found");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
 });
